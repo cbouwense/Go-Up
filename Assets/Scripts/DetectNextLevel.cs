a2c9007a8a460383076a3700f79382f5cc3a4cbf
Christian <bouwensec@gmail.com>
2018-02-10 13:54:25 -0500

    Implemented room changer and restart room key.

---
+++ Assets/Scripts/DetectNextLevel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectNextLevel : MonoBehaviour {
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        RoomController roomController = GameObject.Find("RoomManager").GetComponent<RoomController>();
+
+        if (col.tag == "Birb")
+        {
+            roomController.nextLevel();
+        }
+    }
+
+}
---
+++ Assets/Scripts/RoomController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomController : MonoBehaviour {
+
+    [SerializeField] private int eggCount;
+
+    [SerializeField] private GameObject birb;
+    [SerializeField] private SpawnerController spawner;
+    [SerializeField] private StatsController birbStats;
+
+	void Start () {
+        spawner = GameObject.Find("BirbSpawner").GetComponent<SpawnerController>();
+        birb = spawner.getBirb();
+        birbStats = birb.GetComponent<StatsController>();
+
+        birbStats.setEggMax(eggCount);
+	}
+	
+	void Update () {
+		
+        // Restart Input
+        if (Input.GetKey(KeyCode.R))
+        {
+            Debug.Log("Restarting level");
+            restartLevel();   
+        }
+
+	}
+
+    private void restartLevel()
+    {
+        spawner.respawnBirb();
+    }
+    public void nextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
