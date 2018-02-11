cd6417ff7b0cd3e7e7a2b9d54bf4f29109e66242
Christian <bouwensec@gmail.com>
2018-02-10 20:33:09 -0500

    Added some levels

---
+++ Assets/KillGreg.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillGreg : MonoBehaviour {
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        SpawnerController spawner = GameObject.Find("BirbSpawner").GetComponent<SpawnerController>();
+
+        if (col.tag == "Birb")
+        {
+            spawner.respawnBirb();
+        }
+    }
+
+}
